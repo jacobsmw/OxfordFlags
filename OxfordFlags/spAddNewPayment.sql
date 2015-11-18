5b7780ac48e01605f545ad11ade24ee57bd8039b
paulymd <paulymd@miamioh.edu>
2015-11-18 12:37:31 -0500

    Saved SQL for Procedures
    Saved the SQL statements for the stored procedures where the others were
    saved

---
+++ OxfordFlags/spAddNewPayment.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spAddNewPayment]
+	@BuyerID int,
+	@OrderID int,
+	@AmountPaid float,
+	@PaymentDate date
+
+AS
+	SET NOCOUNT ON
+	BEGIN
+		INSERT INTO [dbo].[Payment] (BuyerID, OrderID, AmountPaid, PaymentDate)
+		VALUES (@BuyerID, @OrderID, @AmountPaid, @PaymentDate)
+	END
